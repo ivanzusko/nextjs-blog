890db39f0ee8599539451efd21b33cd4b3e32034
ivanzusko <ivan.zusko@gmail.com>
2020-11-14 14:56:48 +0100

    chore(ts): fix gray-matter issue

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-// import matter from 'gray-matter';
-import * as matter from 'gray-matter';
+import matter from 'gray-matter';
 import remark from 'remark';
 import html from 'remark-html';
 
